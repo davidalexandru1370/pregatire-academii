41d5f51ba62d53ea88f3d22f4e24f32e59c1d5d5
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-02-08 21:12:28 +0200

    added bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Nav,
+  Navbar,
+  Container,
+  NavDropdown,
+  Button,
+  Alert,
+} from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Nav>
+          <Nav.Item href="#">Acasa</Nav.Item>
+          <Nav.Item href="#">Subiecte</Nav.Item>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
