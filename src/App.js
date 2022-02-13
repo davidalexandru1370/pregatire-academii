35665a9a12dafe947f2e7ca87fe2fa0d15d1c060
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-02-14 00:50:54 +0200

    added hero section

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import React from "react";
 function App() {
   //var font_color_style = {"--color": "#f0f0f0"} as React.CSSProperties;
   return (
-    <div className="App">
+    <div className="App wrapper">
       <Navbar className="bg-dark">
         <Nav className="ms-auto ">
           <Nav.Item href="#" className="p-3 nav-item">
@@ -22,8 +22,29 @@ function App() {
           <Nav.Item href="#" className="p-3 nav-item">
             Subiecte
           </Nav.Item>
+          <Nav.Item className="p-3 nav-item">
+            <img src="" alt="cont utilizator" />
+          </Nav.Item>
         </Nav>
       </Navbar>
+      <section className="hero-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg">
+              <h2 className="hero-title col-4 p-3">
+                Te pregatesti pentru admitere la
+              </h2>
+            </div>
+            <div className="col-lg">
+              <ul className="flip3 list-group-flush">
+                <li className="list-group-item hero-item">politie</li>
+                <li className="list-group-item hero-item">jandarmerie</li>
+                <li className="list-group-item hero-item">pompieri</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
