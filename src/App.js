1061177c1e92dae83ddd81f8fca5a6f748cfc95a
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-04-16 13:08:52 +0300

    create content section with informations

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
 } from "react-bootstrap";
 import React from "react";
 import Changabletext from "./Components/ChangableText";
+import TextCard from "./Components/TextCard";
 
 function App() {
   //var  font_color_style = {"--color": "#f0f0f0"} as React.CSSProperties;
@@ -18,9 +19,11 @@ function App() {
   const firefighters = new AcademyEntity("pompieri", "#f0f0f0");
   const special_forces = new AcademyEntity("jandarmerie", "#f0f0f0");
   var academies = [police, firefighters, special_forces];
-
+  let connection_politie = "http://172.23.240.1:8080/militist.png";
+  let connection_pompier = "http://172.23.240.1:8080/pomper.png";
+  let connection_jandarmerie = "http://172.23.240.1:8080/jendar.png";
   return (
-    <div className="App wrapper">
+    <div className="">
       <Navbar className="bg-dark">
         <Nav className="ms-auto ">
           <Nav.Item href="#" className="p-3 nav-item">
@@ -36,13 +39,60 @@ function App() {
       </Navbar>
       <section className="hero-section">
         <div className="container">
-          <div className="row ">
-            <div className="col-lg">
-              <h2 className="hero-title col-4 p-3">
-                Te pregatesti pentru admitere la
-              </h2>
-              <div className="col-md w-100">
-                <Changabletext text_choices={academies} />
+          <div className="col-lg">
+            <h2 className="hero-title  text-center p-3">
+              Te pregatesti pentru admitere la
+            </h2>
+          </div>
+          <div className="row">
+            <div className="row-lg col d-flex justify-content-evenly">
+              <div className="col-sm d-flex flex-column element">
+                <img
+                  src={`${connection_pompier}`}
+                  alt="pompier"
+                  className="avatar border-end"
+                />
+                <h2 className="hero-item">Pompieri</h2>
+              </div>
+
+              <div className="col-sm d-flex flex-column element">
+                <img
+                  src={`${connection_politie}`}
+                  alt="politie"
+                  className="avatar border-end "
+                />
+                <h2 className="hero-item">Politie</h2>
+              </div>
+              <div className="col-sm d-flex flex-column element">
+                <img
+                  src={`${connection_jandarmerie}`}
+                  alt="jandarmerie"
+                  className=" avatar"
+                />
+                <h2 className="hero-item">Jandarmerie</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row-sm">
+            <div className="hero-title"></div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-info">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 mt-4">
+              <div className="text-center">
+                <h3 className="text-white text-uppercase ">
+                  ce iti putem oferi?
+                </h3>
+              </div>
+            </div>
+            <div className="col-lg-12 mt-5">
+              <div className="card-deck">
+                <TextCard text="Subiecte romana" />
+                <TextCard text="Subiecte matematica" />
+                <TextCard text="Subiecte istorie" />
               </div>
             </div>
           </div>
---
+++ src/Components/TextCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ".././App.css";
+import 'react-bootstrap-icons';
+import { Check } from 'react-bootstrap-icons';
+
+function TextCard(props) {
+  return (
+    <div className="card mb-3">
+    <div className="card-body bg-primary">
+    <Check size={48} className="float-end" />
+        <p className='card-text text-white text-uppercase text-left fs-4 mt-1'>{props.text}</p>
+        
+    </div>
+    </div>
+  )
+}
+
+export default TextCard
