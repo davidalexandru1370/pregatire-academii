afe7b05362684c7ec15b58d95ea0e65aea7f3fe4
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-05-13 23:20:13 +0300

    connect Creeaza cont button from main page with create account page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import { Bootstrap } from 'react-bootstrap-icons';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Register.scss"
 function Register() {
   return (
-    <div>Register</div>
+      <div className="register">
+        <form>
+        <div className="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="text" class="form-control" placeholder='Email' aria-label='Email' aria-describedby='email'></input>
+        </div>
+        </form>
+      </div>
   )
 }
 
