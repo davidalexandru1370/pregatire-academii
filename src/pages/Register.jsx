e6cd00b31b00856359fb7638419aa880df61b5f8
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-05-21 15:44:49 +0300

    create invalid messages for register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,31 +1,101 @@
-import React from 'react';
-import { Bootstrap } from 'react-bootstrap-icons';
+import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Register.scss"
+import "./Register.scss";
+
 function Register() {
+  const [start_validate, set_start_validate] = useState(false);
+  const [name, setname] = useState(false);
+  const form_ref = useRef(null);
+  let name_error_text = "Adresa de email este invalida";
+
+  //check if input name field it is correct
+  const nameInputField = () => {
+    const my_form = form_ref.current;
+    if (String(my_form["name"].value).length < 3) {
+      setname(false);
+      return;
+    }
+
+    setname(true);
+  };
+
+  const checkInputFields = () => {
+    set_start_validate(true);
+    nameInputField();
+  };
+
   return (
-          <div className='register'>
-          <form >
-          <div className="form-group">
-          <div className='mt-5'>
-            <label for= "name">Adresa de email</label>
-            <input type="text" className="form-control" id='name' placeholder='Email' aria-label='email' />
+    <div className="col-sm register">
+      <form ref={form_ref}>
+        <div className=" form-group">
+          <div className="mt-5">
+            <label htmlFor="name">Adresa de email:</label>
+            <input
+              type="text"
+              className={`form-control ${
+                start_validate === false
+                  ? ""
+                  : name === true
+                  ? "valid-field"
+                  : "invalid-field"
+              }`}
+              id="name"
+              placeholder="Email"
+              aria-label="email"
+              onChange={start_validate === true ? nameInputField : null}
+            />
+            <p
+              style={{
+                display: ` ${
+                  start_validate === false
+                    ? "none"
+                    : name === true
+                    ? "none"
+                    : "initial"
+                }`,
+              }}
+              className={`${
+                start_validate === false
+                  ? null
+                  : name === true
+                  ? "valid-field-text"
+                  : "invalid-field-text"
+              }`}
+            >
+              {name_error_text}
+            </p>
           </div>
-          <div className='mt-5'>
-
-            <label for='password'>Parola</label>
-            <input type="text" id='password' className='form-control' placeholder='Parola' aria-label='password' />
+          <div className="mt-5">
+            <label htmlFor="password">Parola:</label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Parola"
+              aria-label="password"
+            />
           </div>
-          <div className='mt-5'>
-
-            <label for="repeatpassword">Repeta parola</label>
-            <input type="text"  id='repeatpassword' className='form-control' placeholder='Repeta parola' aria-label='password'/>
+          <div className="mt-5">
+            <label htmlFor="repeatpassword">Repeta parola:</label>
+            <input
+              type="password"
+              id="repeatpassword"
+              className="form-control"
+              placeholder="Repeta parola"
+              aria-label="password"
+            />
           </div>
-            <button className='register-btn btn mt-3 d-flex justify-content-center '>Creeaza cont</button>
-          </div>
-          </form>
+          <button
+            className="register-btn btn mt-3 d-flex justify-content-center "
+            onClick={checkInputFields}
+            type="button"
+          >
+            Creeaza cont
+          </button>
         </div>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
