a56a6f1d1fb6605ac80fd4aef3a99d320dd25a3c
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-06-07 11:18:22 +0300

    create register password constraints update automatically by the password input field

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,8 +5,13 @@ import "./Register.scss";
 function Register() {
   const [start_validate, set_start_validate] = useState(false);
   const [name, setname] = useState(false);
-  const form_ref = useRef(null);
+  const [password, setPassword] = useState(false);
+  const form_ref = useRef(0);
+  const pass_ref = useRef({});
+  const passwordRegex = new RegExp("(.*[A-Z]+.*[0-9]+)|(.*[0-9].*[A-Z].*)");
   let name_error_text = "Adresa de email este invalida";
+  let password_error_text =
+    "Parola trebuie sa indeplineasca conditiile: \n O litera majuscula \n O cifra\nLungime minima de 5 caractere";
 
   //check if input name field it is correct
   const nameInputField = () => {
@@ -19,9 +24,50 @@ function Register() {
     setname(true);
   };
 
+  const passwordInputFields = () => {
+    const my_form = form_ref.current;
+    const field_caps = pass_ref.current["pass-caps"];
+    const field_digit = pass_ref.current["pass-digit"];
+
+    if (String(my_form["password"].value).length > 5) {
+      const field = pass_ref.current["pass-length"];
+      if (field) {
+        field.style.color = "green";
+      }
+    } else {
+      const field = pass_ref.current["pass-length"];
+      if (field) {
+        field.style.color = "red";
+      }
+    }
+
+    let has_caps = String(my_form["password"].value).match("[A-Z]");
+    if (has_caps !== null) {
+      if (field_caps) {
+        field_caps.style.color = "green";
+      }
+    } else {
+      if (field_caps) {
+        field_caps.style.color = "red";
+      }
+    }
+
+    let has_digit = String(my_form["password"].value).match("[0-9]");
+    if (has_digit !== null) {
+      if (field_digit) {
+        field_digit.style.color = "green";
+      }
+    } else {
+      if (field_digit) {
+        field_digit.style.color = "red";
+      }
+    }
+  };
+
   const checkInputFields = () => {
     set_start_validate(true);
     nameInputField();
+    // passwordInputFields();
   };
 
   return (
@@ -70,10 +116,35 @@ function Register() {
             <input
               type="password"
               id="password"
+              //className="form-control"
               className="form-control"
               placeholder="Parola"
               aria-label="password"
+              onChange={() => passwordInputFields()}
             />
+            <div className={`pass-constr`}>
+              <li
+                id="pass-length"
+                style={{ color: "red" }}
+                ref={(element) => (pass_ref.current["pass-length"] = element)}
+              >
+                Parola trebuie sa contina minim 5 caractere!
+              </li>
+              <li
+                id="pass-caps"
+                style={{ color: "red" }}
+                ref={(element) => (pass_ref.current["pass-caps"] = element)}
+              >
+                Parola trebuie sa contina cel putin o litera mare!
+              </li>
+              <li
+                id="pass-digit"
+                style={{ color: "red" }}
+                ref={(element) => (pass_ref.current["pass-digit"] = element)}
+              >
+                Parola trebuie sa contina cel putin o cifra!
+              </li>
+            </div>
           </div>
           <div className="mt-5">
             <label htmlFor="repeatpassword">Repeta parola:</label>
@@ -87,7 +158,6 @@ function Register() {
           </div>
           <button
             className="register-btn btn mt-3 d-flex justify-content-center "
-            onClick={checkInputFields}
             type="button"
           >
             Creeaza cont
