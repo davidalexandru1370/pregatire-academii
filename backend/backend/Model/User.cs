997b2a142e68c459cec73428dd365a61de2c13a5
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-06-27 23:06:34 +0300

    create files for backend of the site

---
+++ backend/backend/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Model
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string email { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string password { get; set; } = string.Empty;
+    }
+}
