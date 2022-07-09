2673ea114a39f94d7d55f158213f77644f80f2f9
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-07-09 20:58:32 +0300

    test

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+count1=0
+echo "" > README.md
+echo "| C# written lines | ReactJS written lines |" >> README.md
+echo "| :---: | :---: |" >> README.md
+
+for f in $(find backend -maxdepth 3 -type f | grep -E ".*(\.cs)$" ); do
+	lines=`wc -l "$f" | cut -d' ' -f1`
+	count1=$(($count1+$lines))
+done
+		#echo "C# written lines $count1"
+
+count2=0
+for f in $(find src -type f | grep -E ".*(\.jsx?)$"); do
+	lines=`wc -l "$f" | cut -d' ' -f1`
+	count2=$(($count2+$lines))
+done
+
+#	echo "ReactJs written lines $count2"
+echo -n "| ``$count1`` | ``$count2`` |" >> README.md
+
+git push 
