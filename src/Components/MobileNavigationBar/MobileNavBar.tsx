cb2757e43125336f49c4dbb0c0755d660be12d08
David Pop <davidalexandru1370@gmail.com>
2022-09-10 19:27:12 +0300

    added navigation bar functionally on mobile but it does not update by location auto

--- src/Components/MobileNavigationBar/MobileNavBar.tsx
+++ src/Components/MobileNavigationBar/MobileNavBar.tsx
@@ -1,11 +1,24 @@
 import React, { useRef, useState } from "react";
-import "./mobileNavBarStyle.scss"
+//@ts-ignore
+import useCurrentPath from "../../Hooks/useCurrentPath.ts";
+import "./mobileNavBarStyle.scss";
+
+interface INavigationItem {
+    icon: string;
+    name: string;
+    path: string;
+}
 
 const MobileNavBar = () => {
     const clickItemClass: string = "navBarItemClicked";
-    const navigationBarItems = useRef<string[]>(["home", "article", "person"]);
+    const navigationBarItems = useRef<INavigationItem[]>([{ icon: "home", name: "Acasa", path: "/mainpage" }, { icon: "article", name: "Teste", path: "/teste" }, { icon: "person", name: "Cont", path: "/contul_meu" }]);
     const previousLIElement = useRef<HTMLLIElement | null>(null);
     const [distance, setDistance] = useState<number>(0);
+    const location = useCurrentPath();
+
+    const computeDistance = (width: number, elementIndex: number): number => {
+        return Math.trunc(width / navigationBarItems.current.length * elementIndex);
+    }
 
     const toggleClassAtItemClick = (element: HTMLLIElement, index: number) => {
         if (previousLIElement.current === element) {
@@ -14,15 +27,9 @@ const MobileNavBar = () => {
 
         if (previousLIElement.current !== null && previousLIElement.current.classList.contains(clickItemClass) === true) {
             previousLIElement.current.classList.toggle(clickItemClass);
-            console.log(previousLIElement.current.accessKey);
-
-            let previousLIElementKey: number = parseInt(previousLIElement.current.accessKey);
-            if (index >= previousLIElementKey) {
-                setDistance(33 * (index - previousLIElementKey));
-            }
-            else {
-                setDistance(33 * (previousLIElementKey - index));
-            }
+            const navBarWidth: number = 100;
+            // setDistance(Math.trunc(navBarWidth / navigationBarItems.current.length * parseInt(element.accessKey)));
+            setDistance(computeDistance(navBarWidth, parseInt(element.accessKey)));
         }
 
         element.classList.toggle(clickItemClass);
@@ -30,9 +37,19 @@ const MobileNavBar = () => {
     }
 
     const navBarItemCircle: Partial<React.CSSProperties> = {
-        display: "",
+        transition: "transform 0.3s",
         transform: `translateX(${distance}vw)`,
     };
+
+    const initializeNavBar = () => {
+        for (let item of navigationBarItems.current) {
+            if (location === item.path) {
+
+            }
+        }
+    }
+
+    initializeNavBar();
 
     return (
         <div className="mobileNavigationBar">
@@ -40,16 +57,19 @@ const MobileNavBar = () => {
                 <div className="navBarItemCircle" style={navBarItemCircle} ></div>
                 {(() => {
                     return navigationBarItems.current.map((element, index) => {
-                        return <li key={index} accessKey={index.toString()} onClick={(htmlElem) => {
-                            toggleClassAtItemClick(htmlElem.currentTarget, index);
-                        }} className="material-symbols-outlined navBarItem">{element}</li>
+                        return <div className="">
+                            <li key={index} accessKey={(-Math.trunc(navigationBarItems.current.length / 2) + index).toString()} onClick={(htmlElem) => {
+                                toggleClassAtItemClick(htmlElem.currentTarget, index);
+                            }} className="material-symbols-outlined navBarIcon navBarItem">{element.icon}
+                                <span className="navBarItemDesc">{element.name}</span>
+                            </li>
+                        </div>
                     })
-                })()}
+                })()
+                }
             </ul>
         </div>
     )
 }
 
-
-
-export default MobileNavBar;
+export default MobileNavBar; 
