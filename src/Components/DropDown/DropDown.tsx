20eec883ace7259376e365d1558b7326392d9f3c
David Pop <davidalexandru1370@gmail.com>
2022-09-14 23:13:55 +0300

    made dropdown component

--- src/Components/DropDown/DropDown.tsx
+++ src/Components/DropDown/DropDown.tsx
@@ -1,17 +1,51 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 import "./DropDown.scss";
 
-const DropDown: FC<{ className: string, style: React.CSSProperties, options: string[] }> = ({
+const DropDown: FC<{ className?: string, style?: React.CSSProperties, items: string[] }> = ({
     className,
     style,
-    options
+    items
 }) => {
+    const dropDownClickedRef = useRef<HTMLDivElement>(null);
+    const listRef = useRef<HTMLDivElement>(null);
+    const [showItems, setShowItems] = useState<boolean>(false);
+    const [text, setText] = useState<string>("");
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropDownClickedRef.current && !dropDownClickedRef.current.contains(event.target) && showItems === true) {
+                setShowItems(false);
+            }
+        }
+
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        }
+    }, [showItems])
+
     return (
         <div className={`${className}`} style={style}>
-            <div className="field">
+            <div ref={dropDownClickedRef} className="field" onClick={() => {
+                setShowItems(true);
+                listRef.current.style.display = 'block'
+            }}>
+                <span className='dropDownText'>{text}</span>
+                <span className="material-symbols-outlined text-black arrowIcon">
+                    keyboard_arrow_down
+                </span>
 
             </div>
-        </div>
+            <div ref={listRef} className='list'>
+                {
+                    items && items.map((element) => {
+                        return <p className='item' onClick={() => {
+                            setText(element)
+                            listRef.current.style.display = "none";
+                        }}>{element}</p>
+                    })
+                }
+            </div>
+        </div >
     )
 }
 export default DropDown;
--- src/pages/Teste/Teste.tsx
+++ src/pages/Teste/Teste.tsx
@@ -1,14 +1,14 @@
 import "./Teste.scss"
 //@ts-ignore
 import DropDown from "../../Components/DropDown/DropDown.tsx"
-
+import constants from "../../Constants/constants.json";
 export const Teste = () => {
     return (
         <div className="testePage">
-            <div className="leftMenuBar ">
+            <div className="leftMenuBar">
                 <div className="filterCard">
                     <span>Categoria</span>
-                    <DropDown style={{ width: "20vw" }} />
+                    <DropDown style={{ width: "12vw" }} items={constants.academies} />
                 </div>
             </div>
         </div>
