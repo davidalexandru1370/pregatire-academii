fa71268de92f1bd08d9c9ef8ce6feab00457eb42
David Pop <davidalexandru1370@gmail.com>
2022-09-17 00:36:44 +0300

    added test cards

--- src/Components/TestCard/TestCard.tsx
+++ src/Components/TestCard/TestCard.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { FC } from 'react';
+import "./TestCard.scss";
 
-const TestCard = () => {
+const TestCard: FC<{}> = ({ }) => {
     return (
-        <div>TestCard</div>
+        <div className="testCard">
+            <div className="testTitle">Admitere</div>
+            <div className="testContent">
+                <p>Categorie:</p>
+                <p>An:</p>
+                <p>Timp:</p>
+                <p>Punctaj maxim:</p>
+                <p>Punctajul tau:</p>
+            </div>
+            <div className="play d-flex justify-content-center">
+                <button type='button' className='startButton'>Incepe</button>
+            </div>
+
+        </div>
     )
 }
 
--- src/pages/Teste/Teste.tsx
+++ src/pages/Teste/Teste.tsx
@@ -2,27 +2,57 @@ import "./Teste.scss"
 //@ts-ignore
 import DropDown from "../../Components/DropDown/DropDown.tsx"
 import constants from "../../Constants/constants.json";
+//@ts-ignore
+import TestCard from "../../Components/TestCard/TestCard.tsx";
 export const Teste = () => {
     return (
         <div className="testePage">
-            <div className="leftMenuBar">
-                <div className="filterCard">
-                    <div className="cardItem">
-                        <span>Categoria</span>
-                        <DropDown items={constants.academies} />
+            <div className="testeItems">
+                <div className="filterAndSort">
+                    <button type='button'
+                        className="btn btn-primary"
+                    >
+                        Filter
+                    </button>
+                    <p>sort</p>
+                </div>
+                <p>0 rezultate</p>
+            </div>
+            <div className="content">
+                <div className="leftMenuBar">
+                    <div className="filterCard">
+                        <div className="cardItem">
+                            <span>Categoria</span>
+                            <DropDown items={constants.academies} />
+                        </div>
+                        <div className="cardItem">
+                            <span>Anul</span>
+                            <DropDown items={constants.academies} />
+                        </div>
+                        <div className="cardItem d-flex justify-content-center pt-3" >
+                            <button
+                                type="button"
+                                className=" text-white filterButton">
+                                Filtreaza
+                            </button>
+                        </div>
                     </div>
-                    <div className="cardItem">
-                        <span>Anul</span>
-                        <DropDown items={constants.academies} />
+                </div>
+                <div className="rightPart">
+                    <div className="testCards">
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
+                        <TestCard />
                     </div>
-                    <div className="cardItem d-flex justify-content-center pt-3" >
-                        <button
-                            type="button"
-                            className=" text-white filterButton">
-                            Filtreaza
-                        </button>
-                    </div>
-
                 </div>
             </div>
         </div>
