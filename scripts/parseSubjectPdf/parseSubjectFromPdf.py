f35f22a545b94969e58799a653c738996f0b1876
David Alexandru <fanefaraon696@gmail.com>
2022-10-21 23:49:36 +0300

    inserted categories into quiz

--- scripts/parseSubjectPdf/parseSubjectFromPdf.py
+++ scripts/parseSubjectPdf/parseSubjectFromPdf.py
@@ -2,8 +2,8 @@ from fileinput import close
 import PyPDF2
 import re
 from Utilitites import *
-import mysql.connector
-
+import pyodbc
+from uuid6 import uuid6
 pdfName = "C:\\Users\\David\\Desktop\\pregatire-academii\\subjects\\politie\\2021\\Subiecte-Politie-2021.pdf"
 pdfFileObject = open(pdfName, 'rb')
 
@@ -12,8 +12,10 @@ f = open("pdfwrite.txt", "w", 2, encoding='utf8')
 
 answers = []
 parts = []
-header2 = set()
 currentOption = ""
+options = dict()
+connection = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=Academii;Trusted_Connection=yes;')
 
 
 def visitor_body(text, cm, tm, fontDict, fontSize):
@@ -40,14 +42,16 @@ if (True):
         pageObj.extract_text(visitor_text=visitor_body)
         pageObj.extract_text(visitor_text=visitor_header)
         text = formatText("".join(parts))
-        #category = formatText(" ".join(header2))
-        print(currentOption)
-        # print(get_questions_with_answers_from_pagetext(text))
+        if currentOption not in options.keys():
+            cursor = connection.cursor()
+            cursor.execute(
+                """INSERT INTO Quiz(Id,Category,Year,Subject) VALUES(?,?,?,?)""", uuid6(), 0, 2021, currentOption)
+            options[currentOption] = True
+            connection.commit()
         for question in get_questions_with_answers_from_pagetext(text):
-            # f.write(question.get_question())
-            # for answer in question.get_answers():
-            #     f.write(answer.get_answer())
             pass
         parts.clear()
+
+connection.close()
 
 f.close()
