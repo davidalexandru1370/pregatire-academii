0774a2568cee6167fed975c6ffeec1b369f53502
David Alexandru <fanefaraon696@gmail.com>
2022-10-25 23:13:16 +0300

    change a little bit styling for test cards

--- src/Components/TestCard/TestCard.tsx
+++ src/Components/TestCard/TestCard.tsx
@@ -1,23 +1,26 @@
-import { FC } from 'react';
+import { FC, useRef } from "react";
 import "./TestCard.scss";
 
-const TestCard: FC<{}> = ({ }) => {
-    return (
-        <div className="testCard">
-            <div className="testTitle">Admitere</div>
-            <div className="testContent">
-                <p>Categorie:</p>
-                <p>An:</p>
-                <p>Timp:</p>
-                <p>Punctaj maxim:</p>
-                <p>Punctajul tau:</p>
-            </div>
-            <div className="play d-flex justify-content-center">
-                <button type='button' className='startButton'>Incepe</button>
-            </div>
+const TestCard: FC<{}> = ({}) => {
+  const parentPlayButton = useRef<HTMLDivElement>(null);
 
-        </div>
-    )
-}
+  return (
+    <div className="testCard">
+      <div className="testTitle">Admitere</div>
+      <div className="testContent">
+        <p>Categorie:</p>
+        <p>An:</p>
+        <p>Timp:</p>
+        <p>Punctaj maxim:</p>
+        <p>Punctajul tau:</p>
+      </div>
+      <div ref={parentPlayButton} className="play">
+        <button type="button" className="startButton">
+          Incepe
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TestCard;
