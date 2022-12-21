ffe610c4f4e3eb7242a1fc2be29af570f49f340d
David Alexandru <fanefaraon696@gmail.com>
2022-12-21 09:56:37 +0200

    ADDED: answer and footer subcomponents to play quiz component

--- src/Components/PlayQuiz/PlayQuiz.tsx
+++ src/Components/PlayQuiz/PlayQuiz.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 type QuestionComponent = React.FC;
+type AnswerComponent = React.FC;
+type FooterComponent = React.FC;
 type PlayQuizComponent = React.FC<{ children: React.ReactNode }> & {
   Question: QuestionComponent;
+  Answer: AnswerComponent;
+  Footer: FooterComponent;
 };
 
 export const PlayQuiz: PlayQuizComponent = ({ children }): JSX.Element => {
@@ -13,4 +17,14 @@ const Question: QuestionComponent = (): JSX.Element => {
   return <div></div>;
 };
 
+const Answer: AnswerComponent = (): JSX.Element => {
+  return <div></div>;
+};
+
+const Footer: FooterComponent = (): JSX.Element => {
+  return <div></div>;
+};
+
 PlayQuiz.Question = Question;
+PlayQuiz.Answer = Answer;
+PlayQuiz.Footer = Footer;
