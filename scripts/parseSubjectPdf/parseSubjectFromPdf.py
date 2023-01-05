a9f0a5a9dd61bae12fdeb79eb782133b7d6bcdc5
David Alexandru <fanefaraon696@gmail.com>
2023-01-05 19:17:45 +0200

    added barem 2021

--- scripts/parseSubjectPdf/parseSubjectFromPdf.py
+++ scripts/parseSubjectPdf/parseSubjectFromPdf.py
@@ -2,19 +2,17 @@ import asyncio
 import re
 import time
 from fileinput import close
-import aioodbc
 import PyPDF2
 from uuid6 import uuid6
 from Utilitites import *
 import psycopg2
 import psycopg2.extras
 import tabula
-pdfname = "/home/david/Desktop/pregatire-academii/subjects/politie/2020/Subiecte-Politie-2020.pdf"
+pdfname = "C:\\Users\\David\\Desktop\\pregatire-academii\\subjects\\politie\\2020\\Subiecte-Politie-2020.pdf"
 pdffileobject = open(pdfname, 'rb')
 
 pdfReader = PyPDF2.PdfReader(pdffileobject)
 #f = open("pdfwrite.txt", "w", 2, encoding='utf8')
-
 
 
 loop = asyncio.get_event_loop()
@@ -44,17 +42,18 @@ def visitor_header(text, cm, tm, fontDict, fontSize):
 
 
 def main():
-    connection = psycopg2.connect(host="localhost",database="Academii",user='postgres',password='postgres', port="5432")
+    connection = psycopg2.connect(
+        host="localhost", database="Academii", user='postgres', password='postgres', port="5432")
     psycopg2.extras.register_uuid()
     cursor = connection.cursor()
     # cursor = await connection.cursor()
     # # await cursor.execute("DELETE FROM Quiz")
     # # await cursor.execute("DELETE FROM Question")
     # # await cursor.execute("DELETE FROM Answer")
-    cursor.execute('''DELETE FROM "Answer"''',())
-    cursor.execute('''DELETE FROM "Question"''',())
-    cursor.execute('''DELETE FROM "Quiz"''',())
-    
+    cursor.execute('''DELETE FROM "Answer"''', ())
+    cursor.execute('''DELETE FROM "Question"''', ())
+    cursor.execute('''DELETE FROM "Quiz"''', ())
+
     currentQuizId = uuid6()
     connection.commit()
     global index
@@ -69,36 +68,37 @@ def main():
             if currentOption not in options.keys():
                 # print(currentOption)
                 currentQuizId = uuid6()
-                cursor.execute('''INSERT INTO "Quiz" ("Id","Category","Year","Subject") VALUES(%s,%s,%s,%s)''', (currentQuizId, 0, 2021, currentOption))
+                cursor.execute('''INSERT INTO "Quiz" ("Id","Category","Year","Subject") VALUES(%s,%s,%s,%s)''', (
+                    currentQuizId, 0, 2021, currentOption))
                 options[currentOption] = True
                 connection.commit()
             for question in get_questions_with_answers_from_pagetext(text):
                 questionId = uuid6()
                 cursor.execute('''INSERT INTO "Question" ("Id","Text","QuizId") values(%s,%s,%s)''',
-                                     (questionId, question.get_question().strip(), currentQuizId))
+                               (questionId, question.get_question().strip(), currentQuizId))
                 connection.commit()
                 # f.write(question.get_question())
                 for answer in question.get_answers():
-                    cursor.execute('''INSERT INTO "Answer" ("Id","QuestionId","IsCorrect","Text") values(%s,%s,%s,%s)''', (uuid6(), questionId, answer.get_is_correct(), answer.get_answer()))
+                    cursor.execute('''INSERT INTO "Answer" ("Id","QuestionId","IsCorrect","Text") values(%s,%s,%s,%s)''', (uuid6(
+                    ), questionId, answer.get_is_correct(), answer.get_answer()))
                     connection.commit()
                     # f.write(answer.get_answer())
             parts.clear()
         connection.close()
 
+
 def parseBarem():
-    pdf_name = "/home/david/Desktop/pregatire-academii/subjects/politie/2021/Grile-Politie-2021.pdf"
+    pdf_name = "C:\\Users\\David\\Desktop\\Grile-Politie-2020.pdf"
     pdf_file_object = open(pdf_name, 'rb')
     pdf_reader = PyPDF2.PdfReader(pdf_file_object)
-    pageObj = pdf_reader.pages[0]
+    pageObj = pdf_reader.pages[1]
     pageObj.extract_text(visitor_text=visitor_body)
-    text=formatText("".join(parts))
-    table = tabula.read_pdf(pdf_name,pages=1,multiple_tables=True)
-    print(table)
-    #print(text)
-  
+    text = formatText("".join(parts))
+    print(text)
+
 
 if __name__ == "__main__":
     parseBarem()
-    #main()
+    # main()
 
-#f.close()
+# f.close()
